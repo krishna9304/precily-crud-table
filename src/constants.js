dd6ee11785ea5cdb1979123764746c89a521d855
Krishna Mahato <71918441+krishna9304@users.noreply.github.com>
2021-10-13 19:58:12 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 const isDev = process.env.NODE_ENV !== "production";
 module.exports = {
-  BACKEND_URL: isDev ? `http://localhost:8080/` : "https://nourl.com/",
+  BACKEND_URL: isDev ? `http://localhost:8080/` : "https://precily-crud-table-api.herokuapp.com/",
 };
